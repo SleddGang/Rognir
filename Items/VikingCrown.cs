249be42b2246e79741f1b8b0b7f193bc04d7b53d
benjazzy <benjazzy64@gmail.com>
2020-02-13 23:38:27 -0500

    Added Viking Crown which makes undead friendly to the player.

---
+++ Items/VikingCrown.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Rognir.Items
+{
+    class VikingCrown : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Makes undead in the frozen biome friendly.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.accessory = true;
+            item.value = Item.sellPrice(silver: 30);
+            item.rare = ItemRarityID.Blue;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<RognirPlayer>().vikingCrown = true;
+        }
+    }
+}
---
+++ RognirPlayer.cs
@@ -0,0 +1,42 @@
+﻿using Rognir.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Rognir
+{
+    class RognirPlayer : ModPlayer
+    {
+        // Type ids of the monsters that will become friendly.  
+        private static int[] ids = new int[] { -55, -54, -45, -44, -37, -36, -35, -34, -33, -32, -31, -30, -29, -28, -27, -26, 3, 132, 161, 186, 187, 188, 189, 200, 223, 254, 255, 319, 320, 321, 331, 332, 338, 339, 340, 430, 431, 432, 433, 434, 435, 436, 489, NPCID.ArmoredViking,
+                        NPCID.UndeadViking, NPCID.UndeadMiner, -52, -51, -50, -49, -48, -47, -46, -15, 21, 77, 110, 201, 202, 203, 291, 292, 293, 322, 323, 324, 449, 450, 451, 452, 453, 481, 566, 567 };
+
+        public bool vikingCrown = false;
+
+        public override void ResetEffects()
+        {
+            vikingCrown = false;
+        }
+
+        public override bool CanBeHitByNPC(NPC npc, ref int cooldownSlot)
+        {
+            if (vikingCrown)
+            {
+                if (player.ZoneSnow)
+                {
+                    if (Array.Exists(ids, element => element == npc.type))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return base.CanBeHitByNPC(npc, ref cooldownSlot);
+        }
+    }
+}
