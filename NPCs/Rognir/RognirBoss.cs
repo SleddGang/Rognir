3095154c318dcc8834ed07de1e7c8bd469b698e2
benjazzy <benjazzy64@gmail.com>
2020-04-12 22:29:55 -0400

    Increaded max life from 3000 to 4750 and increased defence from 10 to 14.  Increased cooldown between consecutive dashes from 15 to 25 and Rognir will now shoot out three shards in stage one.

--- NPCs/Rognir/RognirBoss.cs
+++ NPCs/Rognir/RognirBoss.cs
@@ -36,7 +36,7 @@ namespace Rognir.NPCs.Rognir
 		private const int rogAttackCoolOne = 105;			// Rognir's attack cooldown for stage one.
 		private const int rogAttackCoolTwo = 75;            // Rognir's attack cooldown for stage two.
 		private const int rogDashLenght = 60;               // Rognir's dash timer to set the lenght of the dash.
-		private const int rogNextDashDelay = 15;			// Sets what the spinTimer will be set to when another dash is going to happen.
+		private const int rogNextDashDelay = 25;			// Sets what the spinTimer will be set to when another dash is going to happen.
 		private const int rogChilledLenghtOne = 120;		// Rognir's chilled buff length for stage one.
 		private const int rogChilledLenghtTwo = 120;        // Rognir's chilled buff length for stage two.
 		private const int rogShardDamage = 10;              // Rognir's ice shard damage.
@@ -101,9 +101,9 @@ namespace Rognir.NPCs.Rognir
 		public override void SetDefaults()
 		{
 			npc.aiStyle = -1;
-			npc.lifeMax = 3000;
+			npc.lifeMax = 4750;
 			npc.damage = 32;
-			npc.defense = 10;
+			npc.defense = 14;
 			npc.knockBackResist = 0f;
 			npc.width = 204;
 			npc.height = 310;
@@ -507,14 +507,10 @@ namespace Rognir.NPCs.Rognir
 			if (Main.netMode != 1)
 			{
 				ShootShard(projVelocity);
-
-				if (stage == 2)
-				{
-					// Shoot out an ice shard 30 degrees offset
-					ShootShard(projVelocity.RotatedBy((Math.PI / 180) * 30));
-					// Shoot out an ice shard 330 degrees offset
-					ShootShard(projVelocity.RotatedBy((Math.PI / 180) * 330));
-				}
+				// Shoot out an ice shard 30 degrees offset
+				ShootShard(projVelocity.RotatedBy((Math.PI / 180) * 30));
+				// Shoot out an ice shard 330 degrees offset
+				ShootShard(projVelocity.RotatedBy((Math.PI / 180) * 330));
 			}
 		}
 
