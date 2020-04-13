870f29c2b6f64c02db4e4e4c542f413ff2eb2b3d
benjazzy <benjazzy64@gmail.com>
2020-04-13 00:23:58 -0400

    Added in spawn info for Rognir.

--- Rognir.cs
+++ Rognir.cs
@@ -28,7 +28,8 @@ namespace Rognir
 					(Func<bool>)(() => RognirWorld.downedRognir),		// downedRognir stores whether Rognir has been defeated or not.  Syncing and saving of this variable is handled in RognirWorld.cs.
 					ModContent.ItemType<Items.Rognir.FrozenCrown>(),	// Sets the spawn item.
 					new List<int> { },									// Collectables.  We don't really have any.
-					new List<int> { ModContent.ItemType<Items.Rognir.RognirBag>(), ModContent.ItemType<Items.FrozenHookItem>(), ModContent.ItemType<Items.RognirsAnchor>()});	// List of Rognir's drops,  Includes the bag.  
+					new List<int> { ModContent.ItemType<Items.Rognir.RognirBag>(), ModContent.ItemType<Items.FrozenHookItem>(), ModContent.ItemType<Items.RognirsAnchor>()},    // List of Rognir's drops,  Includes the bag.  
+					"Use a [i:" + ModContent.ItemType<Items.Rognir.FrozenCrown>() + "] in a snow biome.");	// Sets the message that tells the player how to spawn Rognir.  
 			}
 		}
 	}
